0b7607c10b12378bb92b4af858bbdf4c01c5c1d0
advasileva <alenadvasileva@yandex.ru>
2023-03-05 12:54:35 +0300

    feat: add write substitution

--- src/main/java/org/eolang/speco/Speco.java
+++ src/main/java/org/eolang/speco/Speco.java
@@ -123,7 +123,8 @@ final class Speco {
             .with(new StClasspath("/org/eolang/speco/3-1-add-with.xsl"))
             .with(new StClasspath("/org/eolang/speco/4-1-fence-tuples.xsl"))
             .with(new StClasspath("/org/eolang/speco/5-1-substitute-fence.xsl"))
-            .with(new StClasspath("/org/eolang/speco/6-1-substitute-dominant.xsl"));
+            .with(new StClasspath("/org/eolang/speco/6-1-substitute-dominant.xsl"))
+            .with(new StClasspath("/org/eolang/speco/7-2-substitute-write.xsl"));
         return new Xsline(train).pass(xml);
     }
 
